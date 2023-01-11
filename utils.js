e9085f66bb03f4ee76923a488f30229cec76e18c
Juan Pablo Restrepo <jota.restrepo@sioma.app>
2023-01-11 13:45:35 -0500

    Add new fetch functions

--- utils.js
+++ utils.js
@@ -1,10 +1,27 @@
 import data from "database/data"
-const delay = 1200
+const delay = 0
 
 export function getAllAvocados(){
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(data)
     }, delay); 
+  })
+}
+
+export function getAvocadoById(id){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data[id])
+    }, delay)
+  })
+}
+
+export function getAllAvocadoIds(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const avocadoIds = Object.keys(data)
+      resolve(avocadoIds)
+    }, delay)
   })
 }
