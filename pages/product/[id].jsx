169d1cb84ebe18222e35b5f979a7a93521d5bcef
Juan Pablo Restrepo <jota.restrepo@sioma.app>
2023-01-11 13:46:52 -0500

    Add getStaticPaths and getStaticProps functions to get Avocado product in dynamic page

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,13 +1,24 @@
 import ItemDetail from "@components/items/ItemDetail"
-import { useRouter } from "next/router"
+import { getAllAvocadoIds, getAvocadoById } from "utils"
 
-const PoductPage = () => {
-  const router = useRouter()
-  const { id } = router.query
-
+const PoductPage = ({avocado}) => {
   return (
-    <ItemDetail id={id} />
+    <ItemDetail avocado={avocado}/>
   )
+}
+
+export async function getStaticPaths(){
+  const avocadosIds = await getAllAvocadoIds()
+  const paths = avocadosIds.map(avocadoId => ({params: { id: avocadoId }}))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps(context){
+  const { id } = context.params
+  const avocado = await getAvocadoById(id)
+
+  return { props: { avocado }}
 }
 
 export default PoductPage
