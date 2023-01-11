4c23632b9821117d22b37d2bc1e293d5bc015da1
Juan Pablo Restrepo <jota.restrepo@sioma.app>
2023-01-11 13:45:57 -0500

    Refactor text center in component

--- components/Home.jsx
+++ components/Home.jsx
@@ -3,9 +3,11 @@ import ItemsListPreview from "./items/ItemsListPreview"
 const Home = () => {
   return (
     <div className="w-full">
-      <h1 className="text-4xl font-semibold">Platzi Avo</h1>
-      <div className="w-full">
-        <span>¿Debería comer un avo hoy?</span>
+      <h1 className="flex justify-center w-full text-4xl font-semibold">Platzi Avo</h1>
+      <div className="w-full flex flex-col gap-y-3">
+        <section className="text-center">
+          <span>¿Debería comer un avo hoy?</span>
+        </section>
         <ItemsListPreview />
       </div>
     </div>
