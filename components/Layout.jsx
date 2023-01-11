5cd63a82d72970e731d5f8e76a67dd95df5f296b
Juan Pablo Restrepo <jota.restrepo@sioma.app>
2023-01-10 20:54:47 -0500

    Add layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from "@components/header"
+
+const Layout = ({ children }) => {
+  return (
+    <div className='flex flex-col gap-5 w-full h-screen'>
+      <div className="h-auto">
+        <Header />
+      </div>
+      <div className="w-full text-center flex flex-col px-0 pb-5 sm:px-32">
+        { children }
+      </div>
+    </div>
+  )
+}
+
+export default Layout
