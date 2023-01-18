fbadd4ca38040468f067639be32f047d7ae96b78
Juan Pablo Restrepo <jota.restrepo@sioma.app>
2023-01-18 10:06:46 -0500

    Add cart actions to cartContext file

--- context/CartContext.jsx
+++ context/CartContext.jsx
@@ -1,4 +1,6 @@
-const { createContext, useContext, useState } = require("react");
+import cartReducer from "./cartReducer";
+
+const { createContext, useContext, useState, useReducer, useCallback } = require("react");
 
 const CartContext = createContext()
 
@@ -8,10 +10,62 @@ const CartContextProvider = ({ children }) => {
    * @param cart.cantidad number
    * @param cart.item Object
    */
-  const [cart, setCart] = useState({})
+  const initialState = {}
+
+  const [cart, dispatch] = useReducer(cartReducer, initialState)
+
+  const addCart = useCallback((cantidad, avocado) => {
+    let cartDraft = { ...cart }
+    const cartSize = Object.keys(cartDraft).length
+
+    if (!cartSize) {
+      cartDraft = {
+        ...cartDraft,
+        [avocado.id]: {
+          item: avocado,
+          cantidad,
+        },
+      }
+    }
+
+    if (cartSize) {
+      const previousItem = cartDraft[avocado.id]
+
+      if (previousItem) {
+        cartDraft = {
+          ...cartDraft,
+          [avocado.id]: {
+            ...cartDraft[avocado.id],
+            cantidad: previousItem.cantidad + cantidad,
+          },
+        }
+      }
+
+      if (!previousItem) {
+        cartDraft = {
+          ...cartDraft,
+          [avocado.id]: {
+            item: avocado,
+            cantidad: cantidad,
+          },
+        }
+      }
+    }
+
+    dispatch({
+      type: 'ADD',
+      payload: cartDraft
+    })
+  }, [cart])
+
+  const removeItem = useCallback((avocadoId) => {
+    let cartDraft = {...cart}
+    delete cartDraft[avocadoId]
+    dispatch({type: 'REMOVE_ITEM', payload: cartDraft})
+  }, [cart])
 
   return (
-    <CartContext.Provider value={{cart, setCart}}>
+    <CartContext.Provider value={{cart, addCart, removeItem}}>
       { children }
     </CartContext.Provider>
   )
