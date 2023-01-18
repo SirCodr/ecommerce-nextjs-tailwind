e422fa9a9d43faad2b3afc93b6012a7f00627fb4
Juan Pablo Restrepo <jota.restrepo@sioma.app>
2023-01-18 11:44:25 -0500

    Center all main content for desktop resolution

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,10 @@ const Layout = ({ children }) => {
       <div className="h-auto">
         <Header />
       </div>
-      <div className="w-full flex flex-col px-4 pb-5 sm:items-center">
-        { children }
+      <div className="w-full flex px-4 pb-5 sm:items-center sm:justify-center">
+        <div className="w-full flex flex-col sm:max-w-2xl">
+          { children }
+        </div>
       </div>
     </div>
   )
